5087b6c91bd4fd36bb0712f1f057ca962ef7c64b
Fahad Yousuf™ <86704687+fahadyousuf98@users.noreply.github.com>
2021-07-06 11:33:33 +0500

    Update Dimension_final_after_staging.sql

--- SQL files/datawarehousing/Dimension_final_after_staging.sql
+++ SQL files/datawarehousing/Dimension_final_after_staging.sql
@@ -1,5 +1,5 @@
 create table W_dept_emp_D(
-row_id int,
+row_id int identity(1,1),
 emp_no int,
 dept_no varchar(20),
 from_data date,
@@ -9,7 +9,7 @@ ETL_insert_DT datetime,
 ETL_update_DT datetime);
 
 create table W_dept_manager_D(
-row_id int,
+row_id int identity(1,1),
 emp_no int,
 dept_no varchar(20),
 from_date date,
@@ -19,7 +19,7 @@ ETL_insert_DT datetime,
 ETL_update_DT datetime);
 
 create table W_Employees_D(
-row_id int,
+row_id int identity(1,1),
 emp_no int,
 birt_date date,
 firstname varchar(20),
@@ -31,7 +31,7 @@ ETL_insert_DT datetime,
 ETL_update_DT datetime);
 
 create table W_titles_D(
-row_id int,
+row_id int identity(1,1),
 emp_no int,
 title varchar(20),
 from_date date,
@@ -41,7 +41,7 @@ ETL_insert_DT datetime,
 ETL_update_DT datetime)
 
 create table W_departments_D(
-row_id int,
+row_id int identity(1,1),
 dept_no varchar(20),
 dept_name varchar(20),
 integration_id varchar(50),
