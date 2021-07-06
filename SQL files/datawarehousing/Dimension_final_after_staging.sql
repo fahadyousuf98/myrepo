b44205f975fe798e26339e11ad95d43798d1a9f5
Fahad Yousuf™ <86704687+fahadyousuf98@users.noreply.github.com>
2021-07-06 14:09:02 +0500

    Update Dimension_final_after_staging.sql

--- SQL files/datawarehousing/Dimension_final_after_staging.sql
+++ SQL files/datawarehousing/Dimension_final_after_staging.sql
@@ -2,7 +2,7 @@ create table W_dept_emp_D(
 row_id int identity(1,1),
 emp_no int,
 dept_no varchar(20),
-from_data date,
+from_date date,
 to_date date,
 integration_id varchar(50),
 ETL_insert_DT datetime,
