6e03e1ee468a24b0ac7f65541e63634bfec87107
Fahad Yousuf™ <86704687+fahadyousuf98@users.noreply.github.com>
2021-07-10 08:44:39 +0500

    Create facts_staging.sql
    for our current schema, the salaries table will be our fact table. one thing is that the fact table will have the keys inside it as well, as you can see that both emp_no and dept_no is inside this table.

---
+++ SQL files/datawarehousing/facts_staging.sql
@@ -0,0 +1,9 @@
+create table W_salaries_FS
+(emp_no int,
+dept_no varchar(50),
+salary int,
+from_date int,
+to_date int,
+integration_id varchar(50),
+ETL_insert_DT datetime,
+ETL_update_DT datetime);
