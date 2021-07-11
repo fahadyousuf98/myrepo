29457c32328677312a8716f3cf2347d81b81b386
Fahad Yousuf™ <86704687+fahadyousuf98@users.noreply.github.com>
2021-07-11 14:49:36 +0500

    Update facts_final.sql

--- SQL files/datawarehousing/facts_final.sql
+++ SQL files/datawarehousing/facts_final.sql
@@ -1,10 +1,12 @@
 create table W_salaries_F
-(row_id int identity(1,1),
-emp_no int,
-dept_no varchar(50),
-salary int,
-from_date int,
-to_date int,
+(EMPLOYEES_WID INT,
+DEPARTMENT_WID INT,
+TITLES_WID INT,
+DEPT_EMP_WID INT,
+DEPT_MANAGER_WID INT,
+SALARY INT,
+FROM_DATE_WID INT,
+TO_DATE_WID INT,
 integration_id varchar(50),
 ETL_insert_DT datetime,
 ETL_update_DT datetime);
