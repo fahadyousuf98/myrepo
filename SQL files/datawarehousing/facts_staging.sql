aee8e7bc8710cad15cab2cb65c6a525c702b4b8f
Fahad Yousuf™ <86704687+fahadyousuf98@users.noreply.github.com>
2021-07-12 11:29:24 +0500

    Update facts_staging.sql

--- SQL files/datawarehousing/facts_staging.sql
+++ SQL files/datawarehousing/facts_staging.sql
@@ -2,8 +2,8 @@ create table W_salaries_FS
 (emp_no int,
 dept_no varchar(50),
 salary int,
-from_date int,
-to_date int,
+from_date date,
+to_date date,
 integration_id varchar(50),
 ETL_insert_DT datetime,
 ETL_update_DT datetime);
